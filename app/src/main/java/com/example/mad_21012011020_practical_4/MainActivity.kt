539064dc0a6022bf3026c71cde2d7bae0646dec0
Chintan Aparnathi <chintanaparnathi48@gmail.com>
2023-08-10 11:35:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_21012011020_practical_4/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.mad_21012011020_practical_4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val login :Button=findViewById(R.id.Login)
+        val signup :Button=findViewById(R.id.SingUp)
+        login.setOnClickListener {
+            intent = Intent(this , LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        signup.setOnClickListener(){
+            intent = Intent(this , RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mad_21012011020_practical_4/RegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_21012011020_practical_4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
